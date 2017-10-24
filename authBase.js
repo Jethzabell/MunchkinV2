69506c85e0219e4192ec2dc649278d3e6575f2fb
Jethzabell <jmedina@us.ibm.com>
2017-10-24 00:07:02 -0400

    Cxvc

--- authBase.js
+++ authBase.js
@@ -1,15 +1,3 @@
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBxNiW43QLES6cTIncOrOqTKbYpivgLM4w",
-    authDomain: "veryfirst-e4512.firebaseapp.com",
-    databaseURL: "https://veryfirst-e4512.firebaseio.com",
-    projectId: "veryfirst-e4512",
-    storageBucket: "veryfirst-e4512.appspot.com",
-    messagingSenderId: "962525809825"
-  };
-  firebase.initializeApp(config);
-
 var provider = new firebase.auth.GoogleAuthProvider();
 var user;
 
