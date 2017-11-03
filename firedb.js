73d35fa5c54d669317a92ee5c32e69c4bd70bf57
Jethzabell <jmedina@us.ibm.com>
2017-11-03 18:48:59 -0400

    Scaacasc

--- firedb.js
+++ firedb.js
@@ -55,26 +55,7 @@
       var uid = user.uid;
       var phoneNumber = user.phoneNumber;
       var providerData = user.providerData;
-      user.getIdToken().then(function(accessToken) {
-        document.getElementById('sign-in-status').textContent = 'Signed in';
-        document.getElementById('sign-in').textContent = 'Sign out';
-        document.getElementById('account-details').textContent = JSON.stringify({
-          displayName: displayName,
-          email: email,
-          emailVerified: emailVerified,
-          phoneNumber: phoneNumber,
-          photoURL: photoURL,
-          uid: uid,
-          accessToken: accessToken,
-          providerData: providerData
-       }, null, '  ');
-     });
-    } else {
-        // User is signed out.
-        document.getElementById('sign-in-status').textContent = 'Signed out';
-        document.getElementById('sign-in').textContent = 'Sign in';
-        document.getElementById('account-details').textContent = 'null';
-      }
+
     }, function(error) {
       console.log(error);
   });
